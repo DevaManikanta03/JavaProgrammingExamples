ce06823fe9c6c7c13e07e0dcdf539ae00cd6be6a
DevaManikanta03 <saladevamanikanta2003@gmail.com>
2023-11-01 22:03:38 +0530

    Saving all the files

---
+++ JavaMoreArrayPrograms_1/src/javamorearrayprograms_1/JavaMoreArrayPrograms_1.java
@@ -0,0 +1,9 @@
+package javamorearrayprograms_1;
+
+public class JavaMoreArrayPrograms_1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
--- Simple Java Programs/PracticingJava.java
+++ Simple Java Programs/PracticingJava.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 public class PracticingJava {
+
+    //Swapping... using xor...
     // public static void main(String[] args){
     //     Scanner input = new Scanner(System.in);
     //     System.out.print("a : ");
@@ -13,5 +15,21 @@ public class PracticingJava {
 
     //     System.out.print("A: " + a + "\n");
     //     System.out.print("B: " + b + "\n");
+    // }
+
+    //prime number or not...
+
+    // public static void main(String[] args){
+    //     Scanner input = new Scanner(System.in);
+    //     System.out.print("Enter the number : ");
+    //     int a  = input.nextInt();
+
+    //     for(int i = 2; i < a / 2; i++){
+    //         if(a % i == 0){
+    //             System.out.println("The Entered number is not a prime number");
+    //             System.exit(0);
+    //         }
+    //     }
+    //     System.out.print("Entered number is a prime number!");
     // }
 }
